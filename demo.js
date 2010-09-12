7113f1f767570da5714089d75ad986498e1639c6
Hans Hillen <hans.hillen@gmail.com>
2010-09-13 00:16:12 +0200

    demo dialog focus fix

--- demo.js
+++ demo.js
@@ -168,11 +168,12 @@ $(function() {
     $("#repeat").buttonset();
 
     // dialog
-    $("#sampleDialog").dialog({autoOpen : false, describedBy : "dialogDescription", modal : true,  buttons: { "Ok": function() { $(this).dialog("close"); } } });
+    $("#sampleDialog").dialog({close : function(e){$('#dialogTrigger').focus()} ,autoOpen : false, describedBy : "dialogDescription", modal : true,  buttons: { "Ok": function() { $(this).dialog("close"); } } });
     $('#dialogTrigger').button()
     .click(function() {
         $("#sampleDialog").dialog("open")
             .find(":input").eq(0).focus();
+            return false;
         });
     
     //Checkboxes
